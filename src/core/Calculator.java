430646864cc72202e41f13f5b1962f677c66f9d2
CC <aliezarean@gmail.com>
2021-03-01 21:28:48 +0330

    wrote the subtraction function

--- src/core/Calculator.java
+++ src/core/Calculator.java
@@ -4,4 +4,9 @@ public class Calculator {
     public int addNumbers(int firstNumber,int secondNumber) {
         return firstNumber+secondNumber;
     }
+
+    public int subtractNumbers(int firstNumber,int secondNumber) {
+        return firstNumber-secondNumber;
+    }
+
 }
