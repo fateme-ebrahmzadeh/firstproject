fda9ae0c73d8957ba35dc3cbf1e80afd5196d7af
ladylunafreya <ladyluna.theoracle@gmail.com>
2021-03-01 21:56:07 +0330

    userinterface

---
+++ src/userinterface/userinterface.java
@@ -0,0 +1,34 @@
+package userinterface;
+
+import core.Calculator;
+
+import java.util.Scanner;
+
+public class userinterface {
+    Scanner scan = new Scanner(System.in);
+    Calculator calculator;
+    userinterface(Calculator calculator){
+        this.calculator = calculator;
+    }
+    void run(){
+        System.out.println("hello i'm luna.\n i do sum kind of mathematic task for you \n if you want do Submission write sub and if you want plus write sum and if you want to exit write it.");
+        String input = scan.next();
+        do{
+            if(input.equals("sub")){
+                subFunction();
+            }else if(input.equals("sum")){
+                sumFunction();
+            }
+        }while(!input.equals("exit"));
+    }
+    void subFunction(){
+        int firstNumber = scan.nextInt();
+        int secondNumber = scan.nextInt();
+        System.out.println("subtraction of your numbers is:"+calculator.subtractNumbers(firstNumber,secondNumber));
+    }
+    void sumFunction(){
+        int firstNumber = scan.nextInt();
+        int secondNumber = scan.nextInt();
+        System.out.println("sum of your numbers is :"+calculator.addNumbers(firstNumber,secondNumber));
+    }
+}
